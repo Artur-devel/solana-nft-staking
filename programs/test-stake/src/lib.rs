dcc07aecb468f94662e4c6bcbce8c321cbe9ba68
Nabi <46531267+nabi1020-dev@users.noreply.github.com>
2022-06-06 23:54:51 +0800

    Update lib.rs

--- programs/test-stake/src/lib.rs
+++ programs/test-stake/src/lib.rs
@@ -1,5 +1,5 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{self, SetAuthority, Token, TokenAccount, Transfer};
+use anchor_spl::token::{self, SetAuthority, Token, TokenAccount, Transfer}; 
 use spl_token::instruction::AuthorityType;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
